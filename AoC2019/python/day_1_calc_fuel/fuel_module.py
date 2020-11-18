582c80ca3221692b2ea937b1abd4a335c4a38129
mcshakes <efmccarthy26@gmail.com>
2020-11-17 21:55:15 -0700

    Finished with part one

--- AoC2019/python/day_1_calc_fuel/fuel_module.py
+++ AoC2019/python/day_1_calc_fuel/fuel_module.py
@@ -3,6 +3,7 @@
 import os
 import math
 
+
 class FuelModule:
     def __init__(self, fuel_mass_doc):
         self.incoming_mass = fuel_mass_doc
@@ -11,18 +12,15 @@ class FuelModule:
     def calculate_mass(self):
         with open(os.path.join(os.path.dirname(os.path.abspath(__file__)), self.incoming_mass), 'rt') as in_file:
             data = in_file.readlines()
-            
+
             for mass in data:
-                fuel = int(mass.strip(), 16) / 3
+                fuel = int(mass.strip()) / 3
                 fuel = math.floor(fuel) - 2
                 self.fuel_required.append(fuel)
-
 
     def total_fuel(self):
         x = sum(self.fuel_required)
         print(x)
-
-            
 
 
 f = FuelModule("input.txt")
